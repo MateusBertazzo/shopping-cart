1ded5769b886cefc6b56556b44c6e79c2fb0429d
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-02-14 19:58:42 -0300

    req 3 ok

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionProducts = document.querySelector('.products');
+
+const getChild = async () => {
+  const arrays = await fetchProductsList('computador');
+  arrays.forEach((array) => {
+    const setHtml = createProductElement(array);
+    sectionProducts.appendChild(setHtml);
+  });
+};
+
+getChild();
