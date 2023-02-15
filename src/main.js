60d0f083f8086d087b24a382e4b812cd415dadc6
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-02-15 09:13:41 -0300

    req 4 ok

--- src/main.js
+++ src/main.js
@@ -3,15 +3,31 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 import { createProductElement } from './helpers/shopFunctions';
 
-document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionProducts = document.querySelector('.products');
 
-const getChild = async () => {
+// Apenas remove um elemento passado como parametro
+const removeElement = (e) => {
+  e.remove();
+};
+
+const getElementsApi = async () => {
+  // Crio elemento P para adicionar o carregando... enquanto minha Api não retorna, tentei criar uma function e passar como parametro pro removeElement mas deu ruim, depois vejo isso.
+  const p = document.createElement('p');
+  p.classList.add('loading');
+  p.innerHTML = 'carregando...';
+  sectionProducts.appendChild(p);
+
   const arrays = await fetchProductsList('computador');
   arrays.forEach((array) => {
     const setHtml = createProductElement(array);
     sectionProducts.appendChild(setHtml);
   });
+  // Removo o elemento P depois que a API retorna meus dados
+  removeElement(p);
 };
 
-getChild();
+document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+window.onload = () => {
+  getElementsApi();
+};
